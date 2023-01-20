bcde4036d89d3db2fde6963780d0290aaf88787a
jgolde <jonas.golde@gmail.com>
2023-01-20 17:41:00 +0100

    logging of scores

--- local_domain_extension_fewshot_finetuning.py
+++ local_domain_extension_fewshot_finetuning.py
@@ -13,7 +13,7 @@ def main(args):
         flair.device = f"cuda:{args.cuda_device}"
 
     full_conll = get_corpus(name="conll", map="short", path=args.cache_path)
-
+    average_over_support_sets = []
     for split in range(5):
         try:
             tars_tagger: FewshotClassifier = TARSTagger.load(
@@ -22,7 +22,17 @@ def main(args):
             raise FileNotFoundError(
                 f"{args.cache_path}/flair-models/pretrained-few-shot/{args.transformer}_{args.corpus}_{args.lr}-{args.seed}/final-model.pt - has this model been trained?")
 
-        support_set = ColumnCorpus(data_folder="data/fewshot/conll03/", train_file=f"{split}.txt", sample_missing_splits=False, column_format={0: "text", 1: "ner"})
+        support_set = ColumnCorpus(
+            data_folder="data/fewshot/conll03/",
+            train_file=f"{split}.txt",
+            sample_missing_splits=False,
+            column_format={0: "text", 1: "ner"},
+            label_name_map={
+                    "LOC": "location",
+                    "ORG": "organization",
+                    "PER": "person",
+                    "MISC": "miscellaneous",
+                })
         print(support_set)
 
         dictionary = support_set.make_label_dictionary('ner')
@@ -36,17 +46,26 @@ def main(args):
         trainer = ModelTrainer(tars_tagger, support_set)
 
         trainer.fine_tune(
-            f'{args.cache_path}/flair-models/finetuned-few-shot/{args.transformer}_{args.corpus}_{args.lr}-{args.seed}',
+            f'{args.cache_path}/flair-models/finetuned-few-shot/{args.transformer}_{args.corpus}_{args.lr}-{args.seed}/split_{split}',
             learning_rate=args.lr,
             mini_batch_size=args.bs,
             mini_batch_chunk_size=args.mbs,
             train_with_dev=False,
             monitor_test=False,
-            save_final_model=False
+            save_final_model=False,
+            max_epochs=50
             )
 
         result = tars_tagger.evaluate(data_points=full_conll.test, gold_label_type="ner")
-        print("done")
+        with open(f'{args.cache_path}/flair-models/finetuned-few-shot/{args.transformer}_{args.corpus}_{args.lr}-{args.seed}/split_{split}/result.txt', "w") as f:
+            f.write(result.detailed_results)
+
+        average_over_support_sets.append(result.main_score)
+
+    with open(
+            f'{args.cache_path}/flair-models/finetuned-few-shot/{args.transformer}_{args.corpus}_{args.lr}-{args.seed}/average_result.txt',
+            "w") as f:
+        f.write(f"average micro f1: {(sum(average_over_support_sets) / len(average_over_support_sets))}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
