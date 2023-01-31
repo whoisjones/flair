fbeba1da47363b1a95bdac63c1368e48bb0627fb
jgolde <jonas.golde@gmail.com>
2023-01-31 12:00:40 +0100

    create dir if not present during zero shot evaluation

--- local_domain_extension_zeroshot.py
+++ local_domain_extension_zeroshot.py
@@ -3,6 +3,7 @@ from flair.models import TARSTagger, FewshotClassifier
 from flair.datasets import ColumnCorpus
 from flair.trainers import ModelTrainer
 import flair
+import os
 
 from local_domain_extension_label_name_map import get_corpus, get_label_name_map
 
@@ -26,6 +27,9 @@ def main(args):
     tars_tagger.add_and_switch_to_new_task(task_name="zeroshot-short",
                                        label_dictionary=dictionary,
                                        label_type="ner")
+
+    if not os.path.exists(f'{args.cache_path}/flair-models/finetuned-few-shot/{args.transformer}_{args.pretraining_corpus}_{args.fewshot_corpus}_{args.lr}-{args.seed}/0shot'):
+        os.mkdir(f'{args.cache_path}/flair-models/finetuned-few-shot/{args.transformer}_{args.pretraining_corpus}_{args.fewshot_corpus}_{args.lr}-{args.seed}/0shot/')
 
     result = tars_tagger.evaluate(data_points=full_evaluation_dataset.test, gold_label_type="ner",
                                   out_path=f'{args.cache_path}/flair-models/finetuned-few-shot/{args.transformer}_{args.pretraining_corpus}_{args.fewshot_corpus}_{args.lr}-{args.seed}/0shot/tart_predict_zeroshot.tsv')
