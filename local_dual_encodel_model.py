09e02555be5463a7e56d1ad53882d3459a6d73b2
jgolde <jonas.golde@gmail.com>
2023-06-01 09:31:15 +0200

    added hf training possibility

--- local_dual_encodel_model.py
+++ local_dual_encodel_model.py
@@ -1,18 +1,23 @@
-import argparse
-from pathlib import Path
-import random
+import os
 import json
 import copy
-import itertools
+import glob
+import random
+import argparse
+from pathlib import Path
 from typing import List
 
 import numpy as np
+import numpy.random
 import torch
 import torch.cuda
+import pytorch_lightning as pl
+from transformers import AutoModel, AutoTokenizer, DataCollatorForTokenClassification, TrainingArguments, Trainer, \
+    BatchEncoding, PreTrainedTokenizer
+from datasets import load_dataset
 
 import flair
 from flair.data import Sentence, Dictionary
-from flair.datasets import CONLL_03
 from flair.embeddings import (
     TransformerWordEmbeddings,
     TransformerDocumentEmbeddings, Embeddings
@@ -25,7 +30,7 @@ from torch.utils.data.dataset import Subset
 from local_loner import LONER
 from local_corpora import get_masked_fewnerd_corpus
 
-class BatchedLabelVerbalizerDecoder(torch.nn.Module):
+class MemoryEfficientLabelVerbalizerDecoder(torch.nn.Module):
     def __init__(self, label_embedding: Embeddings, label_dictionary: Dictionary, requires_masking: bool, mask_size: int = 128):
         super().__init__()
         self.label_embedding = label_embedding
@@ -88,7 +93,38 @@ class BatchedLabelVerbalizerDecoder(torch.nn.Module):
 
         return all_scores
 
-def train(args):
+class HfDualEncoder(torch.nn.Module):
+    def __init__(self, labels: List[str], tokenizer: PreTrainedTokenizer, mask_size: int = 128):
+        super(HfDualEncoder, self).__init__()
+        self.encoder = AutoModel.from_pretrained("bert-base-uncased")
+        self.decoder = AutoModel.from_pretrained("bert-base-uncased")
+        self.labels = labels
+        self.num_labels = len(labels)
+        self.tokenizer = tokenizer
+        self.mask_size = mask_size
+        self.loss = torch.nn.CrossEntropyLoss()
+
+    def _filter_labels(self, labels):
+        np_labels = labels.detach().cpu().numpy()
+        unique_labels = np.unique(np.clip(np_labels, a_min=0, a_max=None))
+        additional_ids_needed = self.mask_size - unique_labels.shape[0]
+        sampled_labels = numpy.random.choice(np.arange(0, len(self.labels)), size=additional_ids_needed, replace=False)
+        selected_labels = np.unique(np.concatenate([unique_labels, sampled_labels]))
+        label_description = [self.labels[i] for i in selected_labels]
+        encoded_labels = self.tokenizer(label_description, padding=True, truncation=True, max_length=64, return_tensors="pt").to(labels.device)
+        return encoded_labels, selected_labels
+    def forward(self, input_ids, attention_mask, labels):
+        device = f'cuda:{str(labels.device.index)}'
+        token_hidden_states = self.encoder(input_ids=input_ids, attention_mask=attention_mask)
+        encoded_labels, selected_label_ids = self._filter_labels(labels)
+        label_hidden_states = self.decoder(**encoded_labels)
+        label_cls_token = label_hidden_states.last_hidden_state[:, 0, :]
+        scores = torch.matmul(token_hidden_states.last_hidden_state, label_cls_token.T)
+        logits = torch.zeros(labels.size(0), labels.size(1) , self.num_labels, device=device)
+        logits[:, :, selected_label_ids] = scores
+        return (self.loss(logits.transpose(1, 2), labels),)
+
+def train_flair(args):
     flair.set_seed(args.seed)
 
     if torch.cuda.is_available():
@@ -105,7 +141,14 @@ def train(args):
 
     # Downsample corpus to either 100k or 1M samples
     if not args.corpus_size == "full":
-        samples = 100000 if args.corpus_size =="100k" else 1000000
+        if args.corpus_size == "100k":
+            samples = 100000
+        elif args.corpus_size == "1M":
+            samples = 1000000
+        elif args.corpus_size == "5M":
+            samples = 5000000
+        else:
+            raise ValueError("Invalid corpus size")
         indices = random.sample(range(len(corpus.train)), k=samples)
         corpus._train = Subset(corpus._train, indices)
 
@@ -116,7 +159,7 @@ def train(args):
 
     # Create model
     embeddings = TransformerWordEmbeddings(args.transformer)
-    decoder = BatchedLabelVerbalizerDecoder(label_embedding=TransformerDocumentEmbeddings(args.transformer), label_dictionary=decoder_dict, requires_masking=True, mask_size=args.mask_size)
+    decoder = MemoryEfficientLabelVerbalizerDecoder(label_embedding=TransformerDocumentEmbeddings(args.transformer), label_dictionary=decoder_dict, requires_masking=True, mask_size=args.mask_size)
     model = TokenClassifier(embeddings=embeddings, decoder=decoder, label_dictionary=label_dict, label_type=label_type, span_encoding=args.span_encoding)
 
     trainer = ModelTrainer(model, corpus)
@@ -130,7 +173,120 @@ def train(args):
         save_model_each_k_epochs=1,
     )
 
-def eval(args):
+
+def train_hf(args):
+
+    save_base_path = Path(
+        f"{args.cache_path}/pretrained-dual-encoder-hf/"
+        f"{args.transformer}"
+        f"_LONER_lr-{args.lr}_seed-{args.seed}_mask-{args.mask_size}_size-{args.corpus_size}"
+    )
+
+    pl.seed_everything(args.seed)
+    dataset = load_dataset("json", data_files=glob.glob("/glusterfs/dfs-gfs-dist/goldejon/datasets/loner/jsonl_bio/*"))
+    if not args.corpus_size == "full":
+        if args.corpus_size == "100k":
+            num_samples = 100000
+        elif args.corpus_size == "500k":
+            num_samples = 500000
+        elif args.corpus_size == "1M":
+            num_samples = 1000000
+        else:
+            raise ValueError("Invalid corpus size")
+    else:
+        num_samples = len(dataset["train"])
+    random_numbers = random.sample(range(0, len(dataset["train"]) + 1), num_samples)
+    small_dataset = dataset["train"].select(random_numbers)
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
+    def align_labels_with_tokens(labels, word_ids):
+        new_labels = []
+        current_word = None
+        for word_id in word_ids:
+            if word_id != current_word:
+                # Start of a new word!
+                current_word = word_id
+                label = -100 if word_id is None else labels[word_id]
+                new_labels.append(label)
+            elif word_id is None:
+                # Special token
+                new_labels.append(-100)
+            else:
+                # Same word as previous token
+                label = labels[word_id]
+                # If the label is B-XXX we change it to I-XXX
+                if label % 2 == 1:
+                    label += 1
+                new_labels.append(label)
+
+        return new_labels
+
+    def tokenize_and_align_labels(examples):
+        tokenized_inputs = tokenizer(
+            examples["tokens"], truncation=True, is_split_into_words=True
+        )
+        all_labels = examples["ner_tags"]
+        new_labels = []
+        for i, labels in enumerate(all_labels):
+            word_ids = tokenized_inputs.word_ids(i)
+            new_labels.append(align_labels_with_tokens(labels, word_ids))
+
+        tokenized_inputs["labels"] = new_labels
+        return tokenized_inputs
+
+    train_dataset = small_dataset.map(
+        tokenize_and_align_labels,
+        batched=True,
+        remove_columns=small_dataset.column_names,
+    )
+
+    data_collator = DataCollatorForTokenClassification(tokenizer)
+
+    training_args = TrainingArguments(
+        output_dir=str(save_base_path),
+        overwrite_output_dir=True,
+        do_train=True,
+        per_device_train_batch_size=args.bs,
+        per_device_eval_batch_size=args.bs,
+        learning_rate=args.lr,
+        warmup_ratio=0.1,
+        save_strategy="epoch",
+        save_total_limit=3,
+        seed=args.seed,
+    )
+
+    with open("/glusterfs/dfs-gfs-dist/goldejon/datasets/loner/labelID2label_bio.json", "r") as f:
+        label_mapping = json.load(f)
+
+    labels = []
+    for label in label_mapping.values():
+        if label.startswith("B-"):
+            labels.append(f'begin {label[2:]}')
+        elif label.startswith("I-"):
+            labels.append(f'inside {label[2:]}')
+        elif label == "O":
+            labels.append("outside")
+        else:
+            print("error")
+
+    model = HfDualEncoder(labels=labels, tokenizer=tokenizer, mask_size=args.mask_size)
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=None,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+    )
+
+    trainer.train()
+    model.encoder.save_pretrained(save_base_path / "encoder")
+    model.decoder.save_pretrained(save_base_path / "decoder")
+
+
+
+def eval_fewshot_fewnerd(args):
     flair.device = f"cuda:{args.cuda_device}"
 
     save_base_path = Path(
@@ -228,8 +384,9 @@ def eval(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--do_train", action="store_true")
-    parser.add_argument("--do_eval", action="store_true")
+    parser.add_argument("--train_flair", action="store_true")
+    parser.add_argument("--train_hf", action="store_true")
+    parser.add_argument("--do_eval_fewshot_fewnerd", action="store_true")
     parser.add_argument("--cuda_device", type=int, default=0)
     parser.add_argument("--seed", type=int, default=123)
     parser.add_argument("--cache_path", type=str, default="/glusterfs/dfs-gfs-dist/goldejon/flair-models")
@@ -250,7 +407,9 @@ if __name__ == "__main__":
     parser.add_argument("--pretrained_model_path", type=str, default="/glusterfs/dfs-gfs-dist/goldejon/flair-models/pretrained-dual-encoder/bert-base-uncased_LONER_lr-1e-05_seed-123_mask-128_size-100k/model_epoch_3.pt")
     args = parser.parse_args()
 
-    if args.do_train:
-        train(args)
-    if args.do_eval:
-        eval(args)
+    if args.train_flair:
+        train_flair(args)
+    if args.train_hf:
+        train_hf(args)
+    if args.do_eval_fewshot_fewnerd:
+        eval_fewshot_fewnerd(args)
